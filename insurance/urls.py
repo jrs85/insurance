855473190a7a637453e27526f389bdc71f53008a
José <root+github@josericardo.eti.br>
2018-12-02 11:24:01 -0300

    Add initial index
    Just lists the risk type names

--- insurance/urls.py
+++ insurance/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from rest_framework import routers
 from insurance import views
@@ -28,4 +29,5 @@ urlpatterns = [
         'rest_framework.urls', namespace='rest_framework'
     )),
     url(r'^admin/', admin.site.urls),
+    url('^', TemplateView.as_view(template_name='index.html'))
 ]

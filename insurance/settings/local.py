90046a2b4908fbe87bccb5f6a2b286152470f669
José <root+github@josericardo.eti.br>
2018-12-05 20:52:42 -0300

    Move database settings from base to local settings

--- insurance/settings/local.py
+++ insurance/settings/local.py
@@ -1,3 +1,15 @@
 from .base import *
 
 ALLOWED_HOSTS = ['localhost']
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'test123'
+    }
+}
